98ea72c283881c9ba3e357bbed4b230c0cc860fe
SnIvanova <1snizhanaivanova@gmail.com>
2024-01-10 12:25:35 +0100

    finished thing

--- PG2/pg2/src/component/SingleBook.jsx
+++ PG2/pg2/src/component/SingleBook.jsx
@@ -12,11 +12,11 @@ const SingleBook = ({ book }) => {
     <Card
     style={{
       width: '18rem',
-      border: `2px solid ${selected ? '#ff0000' : '#ccc'}`, // Bordo rosso se selected è true, altrimenti grigio
-      boxShadow: selected ? '0 0 10px rgba(255, 0, 0, 0.5)' : 'none', // Ombra rossa se selected è true, altrimenti nessuna ombra
-      transition: 'border 0.3s, box-shadow 0.3s', // Animazione di transizione per rendere più fluido il cambio di stile
+      border: `2px solid ${selected ? '#ff0000' : '#ccc'}`, 
+      boxShadow: selected ? '0 0 10px rgba(255, 0, 0, 0.5)' : 'none', 
+      transition: 'border 0.3s, box-shadow 0.3s', 
     }}
-    onClick={toggleSelected}
+      onClick={toggleSelected}
   >
     <Card.Img variant="top" src={book.img} alt={`Cover of ${book.title}`} />
     <Card.Body>
